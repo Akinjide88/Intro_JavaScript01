4cef66b254f636b52a3548d2acdfced6c5e0d5e9
Akinjide <jideoscar88@gmail.com>
2022-06-24 00:06:27 +0100

    Introduction to JavaScript

---
+++ main.js
@@ -0,0 +1,7 @@
+let name = "Akinjide";
+let height = "165cm";
+let country = "Nigeria";
+
+console.log("Name:"+ " " + name)
+console.log("Height:"+ " " +height)
+console.log("Country:"+ " " +country)
